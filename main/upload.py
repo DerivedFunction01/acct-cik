ddb95a4268f1de4acf617b51ad328a1e7cd810f5
Denny Li <ytdli08@gmail.com>
2025-10-18 19:18:47 -0400

    add instruction for google drive upload

--- main/upload.py
+++ main/upload.py
@@ -7,6 +7,24 @@ gauth = GoogleAuth()
 gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 
+'''
+
+### \#\# 1. Google Cloud Platform Setup ⚙️
+
+First, you need to enable the Google Drive API and get credentials for your application.
+
+1.  **Create a Google Cloud Project:** If you don't have one already, go to the [Google Cloud Console](https://console.cloud.google.com/) and create a new project.
+2.  **Enable the Google Drive API:** In your project, navigate to **APIs & Services \> Library**. Search for "Google Drive API" and click **Enable**.
+3.  **Create OAuth 2.0 Credentials:**
+      * Go to **APIs & Services \> Credentials**.
+      * Click **+ CREATE CREDENTIALS** and select **OAuth client ID**.
+      * If prompted, configure the **OAuth consent screen**. For an internal tool, you can select **Internal** or **External** and fill in the required app name, user support email, and developer contact information. You don't need to add scopes here.
+      * For the **Application type**, select **Desktop app**. This is crucial as it's designed for flows where the app runs on a local machine (or in this case, a server without a browser).
+      * Give it a name (e.g., "Uploader") and click **Create**.
+      * A window will pop up with your Client ID and Client Secret. Click **DOWNLOAD JSON**. Rename this file to `client_secrets.json`.
+4. Make sure to add the test user
+
+'''
 
 # --- FOLDER LOGIC ---
 folder_name = 'db'
