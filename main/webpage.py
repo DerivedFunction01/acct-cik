a9ea2c4b478aaea91b479a7cb4c07b629f9c4c93
denny <ytdli08@gmail.com>
2025-10-27 06:45:01 -0400

    back to 5

--- main/webpage.py
+++ main/webpage.py
@@ -32,7 +32,7 @@ REPORT_CSV_PATH = "report_data.csv"
 DB_PATH = "web_data.db"
 
 
-SEC_RATE = 7  # requests per second
+SEC_RATE = 5 # requests per second
 SEC_RATE_LIMIT = 1 / SEC_RATE  # requests per second
 CHUNK_SIZE = 100
 NUM_FETCHERS = 1
