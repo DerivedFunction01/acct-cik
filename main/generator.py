eb4ddcbb1ef20fd14a8a12f80e0853070b47ab15
denny <ytdli08@gmail.com>
2025-10-28 13:09:25 -0400

    back to previous year

--- main/generator.py
+++ main/generator.py
@@ -596,7 +596,7 @@ def generate_hedge_paragraph(
         future_year = (
             random.randint(current_year + 1, current_year + 20)
             if has_active_derivative
-            else random.randint(old_year - 1, current_year)
+            else random.randint(old_year - 1, prev_year)
         )
 
         old_notional = generate_value(False)
