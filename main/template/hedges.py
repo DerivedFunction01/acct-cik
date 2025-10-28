5d591f648b5985351cbf62addeb5a96cb5e66a81
denny <ytdli08@gmail.com>
2025-10-28 09:09:07 -0400

    add new additional

--- main/template/hedges.py
+++ main/template/hedges.py
@@ -558,10 +558,11 @@ trend_descriptors = [
 additional_template_patterns = [
     "There was no such activity at {year} related to {swap_type} {hedge_designation}",
     "There was {materiality} activity at {month} {end_day}, {year} related to {swap_type} {hedge_designation}",
-    "{company} did not {verb} any other {gen_swap}", # To not get confused with termination
+    "{company} did not {verb} any other {gen_swap}",  # To not get confused with termination
+    "{company} did not {verb} any new {gen_swap}",  # To not get confused with termination
     "The hedge effectiveness on {swap_type} was {materiality}",
     "The net gain on {swap_type} was {materiality}",
-    "The net loss on {swap_type} was {materiality}"
+    "The net loss on {swap_type} was {materiality}",
 ]
 
 # ------------------------------------------------------------------------------
