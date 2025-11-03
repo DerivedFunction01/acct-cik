bf81f01bb610b83b2d60f6b2d6c7739fb935b249
denny <ytdli08@gmail.com>
2025-11-03 09:36:51 -0500

    add

--- main/defs/table_definitions.py
+++ main/defs/table_definitions.py
@@ -62,4 +62,4 @@ class GenericTable:
         for row_data in self.data_rows:
             all_rows.extend(self._format_row_with_wrapping(row_data, self.widths, self.alignments))
 
-        return f"<TABLE>\n<CAPTION>\n{self.title}\n" + "\n".join(all_rows) + "\n</TABLE>"
+        return f"<TABLE>\n<CAPTION>\n{self.title}\n" + "\n".join(all_rows) + "\n</TABLE>\n"
