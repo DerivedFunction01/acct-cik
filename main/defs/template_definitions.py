55ed3d5d90f936138508c0842a1a50756b25d68b
denny <ytdli08@gmail.com>
2025-11-02 21:11:23 -0500

    update

--- main/defs/template_definitions.py
+++ main/defs/template_definitions.py
@@ -1016,7 +1016,7 @@ class Table:
                     instrument_type=f"Derivatives with maturity of {group.lower()}",
                     reporting_year=self.reporting_year,
                     value_type="notional",
-                    currency=self.currency_symbol,
+                    currency=self.money_unit(),
                     sentence_type="summary", # type: ignore
                     aggregate=True,
                 ))
@@ -1192,7 +1192,7 @@ class Table:
                 notional_str=reclass_str, year=year,
                 instrument_type=f"AOCI reclassification for {inst.instrument_type}",
                 reporting_year=self.reporting_year, value_type="fair_value",
-                currency=self.currency_symbol, sentence_type="individual",
+                currency=self.money_unit(), sentence_type="individual",
             ))
 
         if len(rows) <= 3:
