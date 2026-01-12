c3639ac1b84f4674d63f2a5267eacdfa066a4056
= <=>
2026-01-12 18:04:04 -0500

    update

--- main/database_filter/prefilter_tagging.py
+++ main/database_filter/prefilter_tagging.py
@@ -409,6 +409,8 @@ def tag_paragraph(text: str, reporting_year: int, is_nst: bool = False) -> str:
                 reason = NoiseReason.RISK
             elif is_pnl(masked, context_only=True): # PNL is tricky
                 reason = NoiseReason.PNL
+            else:
+                reason = NoiseReason.CTX
 
         if not reason:
             reason = get_temporal_noise_reason(masked, reporting_year)
