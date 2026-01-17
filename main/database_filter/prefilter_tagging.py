467963c403623296f697937f2b6d64f7b99edda1
= <=>
2026-01-17 11:01:11 -0500

    update counterpparty

--- main/database_filter/prefilter_tagging.py
+++ main/database_filter/prefilter_tagging.py
@@ -150,7 +150,7 @@ COUNTERPARTY_POLICY_TERMS = [
     r"financial\s+institutions",
     r"master\s+netting",
     r"collateral\s+requirements",
-    r"concentration\s+of\s+credit",
+    r"concentrations?\s+of\s+credit",
     r"non[- ]performance",
     r"nonperformance",
     r"monitoring",
