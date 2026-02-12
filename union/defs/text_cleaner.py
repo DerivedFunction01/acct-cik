acb9b0e014b35a2a595202dfa92c4b8c3fcf60b7
= <=>
2026-02-12 12:25:46 -0500

    update

--- union/defs/text_cleaner.py
+++ union/defs/text_cleaner.py
@@ -1035,6 +1035,8 @@ class ContextualNumberCleaner:
             r"regions?",
             r"lodges?",
             r"councils?",
+            r"chapters?",
+            r"branches?",
             r"propositions?", # California proposition
         ]
         union_id_pattern = build_alternation(union_identifiers)
