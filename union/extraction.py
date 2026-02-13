760fd676ddc425723881b7f58636e02a923f6672
= <=>
2026-02-12 23:35:12 -0500

    update

--- union/extraction.py
+++ union/extraction.py
@@ -175,7 +175,7 @@ UNION_DENOMINATOR_REGEX = build_regex(
 
 DIVERSITY_REGEX = build_regex(DIVERSITY_TERMS)
 SUBSET_REGEX = build_regex([
-    r"of\s+(?:which|whom)",
+    r"of\s+(?:which|whom|these|those)",
     r"includ(?:ing|es)",
     r"compris(?:ing|es?|ed)",
     r"with",
