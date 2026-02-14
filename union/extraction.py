49f14f0d5c127edaa3909f9dd35a78f7da56de73
= <=>
2026-02-14 17:34:45 -0500

    fix

--- union/extraction.py
+++ union/extraction.py
@@ -418,7 +418,7 @@ QUALITATIVE_MEMBERSHIP = [
 # Triggers only when needed as last resort, to avoid converting 100% x COUNT -> Qualitative.value
 QUALITATIVE_ALL_TERMS = [
     QualitativeTerm(
-        core_terms=[r"all", "entirety"],
+        core_terms=[r"all(?!-)", "entirety"],
         positive_pct=100.0,
         negated_pct=None,
         requires_suffix=False,
